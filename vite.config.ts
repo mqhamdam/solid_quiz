a2abfb1419d38a82a6250ae00940989955fa97bb
Hamdam <mq.hamdam@gmail.com>
2024-07-30 16:05:44 +0900

    edit vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/solid_quiz/',
+  server: {
+    host: true ,
+
+  },
+  build:
+  {
+    outDir: '../dist', // Output in the dist/ folder
+    emptyOutDir: true, // Empty the folder first
+    sourcemap: true // Add sourcemap
+  },
 })
